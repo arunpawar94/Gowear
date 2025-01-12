9ef73519b75c802b7c974d3f629363c28eb1c321
arunpawar94 <arunpawar94@gmail.com>
2025-01-12 15:20:00 +0530

    make connection with database

--- server/src/app.ts
+++ server/src/app.ts
@@ -1,11 +1,14 @@
-import express, {Request, Response} from "express"
+import express, {Request, Response} from "express";
+import connectDB from './config/db';
 
 const app = express();
+
+connectDB();
 
 app.get("/", (req: Request, res: Response) => {
     res.send("Hi from server side again");
 });
 
-app.listen(5000, ()=> {
+app.listen(process.env.PORT, ()=> {
     console.log("Server is running")
 });
