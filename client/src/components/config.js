4731f5144dbd627d19055f96afe3e87885cf01d9
arunpawar94 <arunpawar94@gmail.com>
2025-04-11 14:00:51 +0530

    design the UI of footer

--- client/src/components/config.js
+++ client/src/components/config.js
@@ -1,1 +1,11 @@
-exports.logIn = "Log In"
+exports.logIn = "Log In";
+exports.contactUs = "Contact Us";
+exports.footerContactUsAddress = {
+    address: "123 Main Street, Anytown, USA",
+    email: "info@example.com",
+    phone: "+1 123 456 78901"
+};
+exports.followUs = "Follow Us";
+exports.copyright = "Copyright";
+exports.copyrightSymbol = "©";
+exports.gowear = "Gowear";
--- client/src/config/assets.tsx
+++ client/src/config/assets.tsx
@@ -1,1 +1,3 @@
-export const bannerImage = require("../assets/bannerImage.jfif")
+export const bannerImage = require("../assets/bannerImage.jfif");
+export const gowearLogo = require("../assets/gowearLogoTransparent.png");
+export const gowearLogoImageCrope = require("../assets/gowearImageCrope.png");
