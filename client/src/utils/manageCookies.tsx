9982a3f065c3d9a42f58663bd4ac1e363a8862e7
arunpawar94 <arunpawar94@gmail.com>
2025-07-28 13:56:02 +0530

    make dummy comment

--- client/src/utils/manageCookies.tsx
+++ client/src/utils/manageCookies.tsx
@@ -1,5 +1,5 @@
 const getCookie = (name: string): string | null => {
-    console.log("@@@3", name)
+    console.log("@@@3in", name)
   const match = document.cookie.match(new RegExp("(^| )" + name + "=([^;]+)"));
   return match ? match[2] : null;
 };
