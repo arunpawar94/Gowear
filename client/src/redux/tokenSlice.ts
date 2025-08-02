f61b8d0687980f35ee42863829166cd3caa4a9b1
arunpawar94 <arunpawar94@gmail.com>
2025-08-02 14:57:11 +0530

    fixes the refresh token cookie on reload

--- client/src/redux/tokenSlice.ts
+++ client/src/redux/tokenSlice.ts
@@ -2,10 +2,12 @@ import { createSlice, PayloadAction } from "@reduxjs/toolkit";
 
 interface TokenInterface {
   token: string | null;
+  intervalId: ReturnType<typeof setInterval> | null;
 }
 
 const initialState: TokenInterface = {
   token: null,
+  intervalId: null,
 };
 
 const accessToken = createSlice({
@@ -15,8 +17,14 @@ const accessToken = createSlice({
     setAccessToken: (state, action: PayloadAction<string | null>) => {
       state.token = action.payload;
     },
+    setIntervalId: (
+      state,
+      action: PayloadAction<ReturnType<typeof setInterval> | null>
+    ) => {
+      state.intervalId = action.payload;
+    },
   },
 });
 
-export const { setAccessToken } = accessToken.actions;
+export const { setAccessToken, setIntervalId } = accessToken.actions;
 export default accessToken.reducer;
