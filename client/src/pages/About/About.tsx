578e285952276284d530a4cf154bf0ee65878c9d
arunpawar94 <arunpawar94@gmail.com>
2025-10-15 13:25:43 +0530

    design the UI of about screen and fix the drawer position

--- client/src/pages/About/About.tsx
+++ client/src/pages/About/About.tsx
@@ -1,5 +1,45 @@
+import { Box, Typography } from "@mui/material";
 import configJSON from "./config";
+import { CSSProperties } from "react";
 
 export default function Checkout() {
-  return <div>{configJSON.about}</div>;
+  return (
+    <Box sx={webStyle.mainBox}>
+      <Typography style={webStyle.mainHeading}>{configJSON.aboutUs}</Typography>
+      <Typography style={webStyle.paraOne}>{configJSON.paraOne}</Typography>
+      <Typography style={webStyle.visionHeading}>
+        {configJSON.ourVisionIs}
+      </Typography>
+      <Typography style={webStyle.visionParaOne}>
+        {configJSON.visionParaOne}
+      </Typography>
+      <Typography>{configJSON.visionParaTwo}</Typography>
+    </Box>
+  );
 }
+
+const webStyle = {
+  mainBox: {
+    padding: "30px 30px 40px",
+    "@media(max-width: 400px)": {
+      padding: "30px 10px",
+    },
+  },
+  mainHeading: {
+    fontSize: "30px",
+    textDecoration: "underline",
+    textAlign: "center",
+  } as CSSProperties,
+  paraOne: {
+    marginTop: "20px",
+  },
+  visionHeading: {
+    fontSize: "22px",
+    marginTop: "10px",
+  },
+  visionParaOne: {
+    fontWeight: "bold",
+    marginTop: "10px",
+    marginBottom: "10px",
+  },
+};
